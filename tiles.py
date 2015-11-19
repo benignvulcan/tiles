e4382f88a58552dfe677286fbb1f8a5478b5d2ab
Marty White <lmw22@cornell.edu>
2015-11-19 13:24:33 -0500

    got basic drag & rotate working

--- tiles.py
+++ tiles.py
@@ -28,6 +28,16 @@ def RegularPolygon(sides=6, size=None, r=None, rotate=0.0):
                             )
                         )
 
+def FormatQTransform(xform):
+  fmtDict = {}
+  fmtFields = []
+  for row in (1,2,3):
+    for col in (1,2,3):
+      attr = "m{}{}".format(row,col)
+      fmtDict[attr] = getattr(xform, attr)()
+      fmtFields.append("{"+attr+"}")
+  return "[{}]".format(','.join(fmtFields).format(**fmtDict))
+
 #====
 
 # constants for different dragging states
@@ -61,29 +71,37 @@ class DragXformer(object):
   def StartDrag(self, mouse_pos=None, drag_type=None, xform_center=None):
     'Some kind of dragging has started.  Remember the starting position of mouse.'
     self._mouse_start = mouse_pos
+    print('DragXformer.StartDrag(mouse_pos={}, drag_type={}, xform_center={})'.format(mouse_pos,drag_type,xform_center))
     if not drag_type is None:
       self._drag_type = drag_type
     if not xform_center is None:
       self._xform_center = xform_center
     else: print('DragXformer.StartDrag: new xform_center =', xform_center)
-    self._start_vector = self._mouse_start - self._xform_center
+    #self._start_vector = self._mouse_start - self._xform_center
+    self._start_vector = QtCore.QLineF(self._xform_center, self._mouse_start)
   def UpdateDrag(self, mouse_pos):
     'Mouse has moved while dragging.  Update the drag transformation.'
-    #print 'UpdateDrag(%s)' % (mouse_pos,)
+    print('DragXformer.UpdateDrag({})'.format(mouse_pos))
     if self._drag_type in (DRAG_XLATE, DRAG_PAN):
       #dm = mouse_pos - self._mouse_start
       #print 'dm =', dm
       delta = mouse_pos - self._mouse_start
       self._drag_xform = QtGui.QTransform.fromTranslate(delta.x(), delta.y())
-      #print '_drag_xform =', self._drag_xform
+      print('DragXformer._drag_xform = {}'.format(FormatQTransform(self._drag_xform)))
     elif self._drag_type in (DRAG_ROTATE, DRAG_ROLL):
-      v2 = mouse_pos - self._xform_center
-      theta = rel_angle(self._start_vector, v2)
+      #v2 = mouse_pos - self._xform_center
+      v2 = QtCore.QLineF(self._xform_center, mouse_pos)
+      #theta = rel_angle(self._start_vector, v2)
+      theta = self._start_vector.angleTo(v2)
+      print(v2, theta)
       if self._constrain_angle:
-        q = self._angular_resolution / (2*pi)
-        theta = trunc(.5+theta*q) / q
-      m0 = composeAffineMatrices(XlateMatrix(-self._xform_center), RotationMatrix(theta))
-      self._drag_xform = composeAffineMatrices(m0, XlateMatrix(self._xform_center))
+        q = self._angular_resolution / 360 #(2*math.pi)
+        #theta = math.trunc(.5+theta*q) / q
+        theta = self._angular_resolution * round(float(theta) / self._angular_resolution)
+      # TODO: find Qt equivalent of affine matrix composition
+      #m0 = composeAffineMatrices(XlateMatrix(-self._xform_center), RotationMatrix(theta))
+      #self._drag_xform = composeAffineMatrices(m0, XlateMatrix(self._xform_center))
+      self._drag_xform.rotate(theta)
     elif self._drag_type in (DRAG_SCALE, DRAG_ZOOM):
       v2 = mouse_pos - self._xform_center
       factor = magnitude(v2) / magnitude(self._start_vector)
@@ -107,34 +125,80 @@ class TileItem(QtWidgets.QAbstractGraphicsShapeItem):
     self.setPen(QtGui.QPen(QtCore.Qt.black, 3))
     self.setBrush(QtCore.Qt.yellow)
     self.selectionPen = QtGui.QPen(QtCore.Qt.red, 3)
+    self._drag_type = None
     self._drag_xformer = DragXformer()
-  def mousePressEvent(self, event):
-    if event.button() != QtCore.Qt.LeftButton:
-      event.ignore()
-      return
-    #self.setCursor(QtCore.Qt.ClosedHandCursor)
+  def center(self):
+    return self.boundingRect().center()
+  def sceneCenter(self):
+    return self.sceneTransform().map(self.center())
+  def mousePressEvent(self, gsMouseEvt):
+    print("TileItem.mousePressEvent(): scenePos={}".format(gsMouseEvt.scenePos()))
+    if gsMouseEvt.button() == QtCore.Qt.LeftButton:
+      self._drag_type = DRAG_XLATE
+      return super().mousePressEvent(gsMouseEvt)
     self.scene().clearSelection()
+    #self.setCursor(QtCore.Qt.ClosedHandCursor)
     self.setSelected(True)
-    self._drag_xformer.StartDrag(event.pos(), DRAG_XLATE, event.pos())
-    event.accept()
-  def mouseMoveEvent(self, event):
-    if not event.buttons():
-      event.ignore()
+    if gsMouseEvt.button() == QtCore.Qt.LeftButton:
+      self._drag_type = DRAG_XLATE
+      self._drag_start_pos = gsMouseEvt.scenePos()
+    elif gsMouseEvt.button() == QtCore.Qt.RightButton:
+      self._drag_type = DRAG_ROTATE
+      self._drag_start_xform = self.transform()
+      self._drag_start_pos = gsMouseEvt.scenePos()
+      self._drag_start_vector = QtCore.QLineF(self.sceneCenter(), self._drag_start_pos)
+    elif gsMouseEvt.button() == QtCore.Qt.MiddleButton:
+      self._drag_type = DRAG_SCALE
+    else:
+      gsMouseEvt.ignore()
+      print("TileItem.mousePressEvent() ignoring non LeftButton")
       return
-    event.accept()
-    if self._drag_xformer.GetDragType() == DRAG_PAN:
-      self._drag_xformer.UpdateDrag(event.pos())
-    #if QtCore.QLineF(QtCore.QPointF(event.screenPos()), QtCore.QPointF(event.buttonDownScreenPos(QtCore.Qt.LeftButton))).length() < QtWidgets.QApplication.startDragDistance(): return
+    self._drag_xformer.StartDrag(gsMouseEvt.scenePos(), self._drag_type, gsMouseEvt.scenePos())
+    #self.resetTransform()
+    #self.setTransform( self._drag_xformer.Transform() )
+    gsMouseEvt.accept()
+  def mouseMoveEvent(self, gsMouseEvt):
+    print("TileItem.mouseMoveEvent(): scenePos={}".format(gsMouseEvt.scenePos()))
+    if self._drag_type == DRAG_XLATE:
+      return super().mouseMoveEvent(gsMouseEvt)
+    if not gsMouseEvt.buttons():
+      gsMouseEvt.ignore()
+      print("TileItem.mouseMoveEvent() ignoring NO BUTTONS")
+      return
+    #if self._drag_xformer.GetDragType() == DRAG_XLATE:
+    #if QtCore.QLineF(QtCore.QPointF(gsMouseEvt.screenPos()), QtCore.QPointF(gsMouseEvt.buttonDownScreenPos(QtCore.Qt.LeftButton))).length() < QtWidgets.QApplication.startDragDistance(): return
+    if self._drag_type == DRAG_XLATE:
+      self.setPos(gsMouseEvt.scenePos() - self._drag_start_pos)
+      gsMouseEvt.accept()
+      return
+    if self._drag_type == DRAG_ROTATE:
+      move_vector = QtCore.QLineF(self.sceneCenter(), gsMouseEvt.scenePos())
+      theta = move_vector.angleTo(self._drag_start_vector)
+      self.setTransform(QtGui.QTransform(self._drag_start_xform).rotate(theta))
+      gsMouseEvt.accept()
+      return
+    self._drag_xformer.UpdateDrag(gsMouseEvt.scenePos())
+    self.setTransform( self._drag_xformer.Transform() )
     if False:
       mimedata = QtCore.QMimeData()
       mimedata.setData(MIME_TYPE_SVG, dummySVG)
-      drag = QtGui.QDrag(event.widget())
+      drag = QtGui.QDrag(gsMouseEvt.widget())
       drag.setMimeData(mimedata)
       drag.start()
       drag.exec_()
     #self.setCursor(QtCore.Qt.OpenHandCursor)
-  def mouseReleaseEvent(self, event):
-    self._drag_xformer.UpdateDrag(event.pos())
+    gsMouseEvt.accept()
+  def mouseReleaseEvent(self, gsMouseEvt):
+    print("TileItem.mouseReleaseEvent(): scenePos={}".format(gsMouseEvt.scenePos()))
+    if self._drag_type == DRAG_XLATE:
+      self._drag_type = None
+      return super().mouseReleaseEvent(gsMouseEvt)
+    elif self._drag_type == DRAG_ROTATE:
+      self._drag_type = None
+      gsMouseEvt.accept()
+      return
+    self._drag_xformer.UpdateDrag(gsMouseEvt.scenePos())
+    self.setTransform( self._drag_xformer.Transform() )
     self._drag_xformer.ResetDrag()
     #self.setCursor(QtCore.Qt.OpenHandCursor)
 
@@ -162,8 +226,15 @@ class PolygonTileItem(TileItem):
 
 class RegularPolygonTileItem(PolygonTileItem):
   def __init__(self, parent=None, sides=6, size=8):
-    g = RegularPolygon(sides=6, size=size)
+    g = RegularPolygon(sides=sides, size=size)
     PolygonTileItem.__init__(self, parent, g)
+
+def Arrowhead(size=8):
+  "Return a concave symmetrical quadrilateral"
+  # TODO: calibrate to unit scale & nice angles
+  # (concavity should be right angle, points should snap to square grid?)
+  return PolygonTileItem(polygon=QtGui.QPolygonF(map(lambda p: QtCore.QPointF(*p),
+    [(0,.25*size),(.75*size,size),(0,-size),(-.75*size,size),(0,.25*size)])))
 
 #====
 
@@ -174,6 +245,7 @@ class TileGraphicsView(QtWidgets.QGraphicsView):
     self._drag_xformer = DragXformer()
     self._mouse_pos_previous = None
     self.setAcceptDrops(True)
+    #self.dragMode = QtWidgets.QGraphicsView.ScrollHandDrag  # what to do wtih mouse clicks not caught by a GraphicsItem
   ZOOM_FACTOR=1.5
   def Zoom(self, f=None):
     if f is None: f = self.ZOOM_FACTOR
@@ -185,46 +257,52 @@ class TileGraphicsView(QtWidgets.QGraphicsView):
     self.Zoom(self.ZOOM_FACTOR)
   def ZoomOut(self):
     self.Zoom(1/self.ZOOM_FACTOR)
-  def wheelEvent(self, event):
-    #print "wheelEvent.delta() =", event.delta() / 8.0
-    f = self.ZOOM_FACTOR ** (event.angleDelta().y() / 120.0)  # 120 = 15 degrees = 1 typical wheel step
+  def wheelEvent(self, mouseEvt):
+    #print "wheelEvent.delta() =", mouseEvt.delta() / 8.0
+    f = self.ZOOM_FACTOR ** (mouseEvt.angleDelta().y() / 120.0)  # 120 = 15 degrees = 1 typical wheel step
     self.Zoom(f)
-  def mousePressEvent(self, event):
-    QtWidgets.QGraphicsView.mousePressEvent(self, event)
-    if event.isAccepted():
+  def mousePressEvent(self, mouseEvt):
+    # Use overridden parent to do standard event multiplexing to scene items:
+    QtWidgets.QGraphicsView.mousePressEvent(self, mouseEvt)
+    if mouseEvt.isAccepted():
+      # Parent mousePressEvent found a GraphicsItem to handle this mouseEvt.
       return
-    if event.button() == QtCore.Qt.MidButton:
-      self._drag_xformer.StartDrag(event.pos(), DRAG_PAN, event.pos())
-      self._mouse_pos_previous = event.pos()
+    # User is trying to mouse on background.
+    if mouseEvt.button() == QtCore.Qt.MidButton:
+      # Middle button pans scene
+      self._drag_xformer.StartDrag(mouseEvt.pos(), DRAG_PAN, mouseEvt.pos())
+      self._mouse_pos_previous = mouseEvt.pos()
       assert self._mouse_pos_previous is not None
-      event.accept()
-  def mouseMoveEvent(self, event):
+      mouseEvt.accept()
+    # otherwise ignore
+  def mouseMoveEvent(self, mouseEvt):
     if self._drag_xformer.GetDragType() == DRAG_PAN:
-      delta = event.pos() - self._mouse_pos_previous
+      self._drag_xformer.UpdateDrag(mouseEvt.pos())  # DRAG XFORMER NOT ACTUALLY USED YET
+      delta = mouseEvt.pos() - self._mouse_pos_previous
       hbar = self.horizontalScrollBar()
       hbar.setValue(hbar.value() - delta.x())
       vbar = self.verticalScrollBar()
       vbar.setValue(vbar.value() - delta.y())
-      self._mouse_pos_previous = event.pos()
-      event.accept()
+      self._mouse_pos_previous = mouseEvt.pos()
+      mouseEvt.accept()
       return
-    QtWidgets.QGraphicsView.mouseMoveEvent(self, event)
-  def mouseReleaseEvent(self, event):
+    QtWidgets.QGraphicsView.mouseMoveEvent(self, mouseEvt)
+  def mouseReleaseEvent(self, mouseEvt):
     if self._drag_xformer.GetDragType() == DRAG_PAN:
-      self._drag_xformer.UpdateDrag(event.pos())
+      self._drag_xformer.UpdateDrag(mouseEvt.pos())
       self._drag_xformer.ResetDrag()
-      event.accept()
+      mouseEvt.accept()
     else:
-      QtWidgets.QGraphicsView.mouseReleaseEvent(self, event)
-  def dragEnterEvent(self, event):
+      QtWidgets.QGraphicsView.mouseReleaseEvent(self, mouseEvt)
+  def dragEnterEvent(self, dragEnterEvt):
     print("TileGraphicsView.dragEnterEvent()")
-    print([s for s in event.mimeData().formats()])
-    if event.mimeData().hasFormat(MIME_TYPE_SVG):
-      event.accept()
-  #def dragMoveEvent(self, event): pass
-  def dropEvent(self, event):
+    print([s for s in dragEnterEvt.mimeData().formats()])
+    if dragEnterEvt.mimeData().hasFormat(MIME_TYPE_SVG):
+      dragEnterEvt.accept()
+  #def dragMoveEvent(self, dragMoveEvt): pass
+  def dropEvent(self, dropEvt):
     print("TileGraphicsView.dropEvent()")
-    event.acceptProposedAction()
+    dropEvt.acceptProposedAction()
 
 #====
 
@@ -249,6 +327,10 @@ class MagneticTilesMainWindow(Ui_MagneticTilesMainWindow, QtWidgets.QMainWindow)
       x.setBrush(QtGui.QColor.fromHsv(i*60, 255, 255))
       x.moveBy(i*10, i*10)
       self.scene.addItem(x)
+    arrowhead = Arrowhead(12)
+    arrowhead.setBrush(QtGui.QColor.fromHsv(50,255,255))
+    arrowhead.moveBy(-20,-20)
+    self.scene.addItem(arrowhead)
 
     self.graphicsView.setAcceptDrops(True)
     self.graphicsView.setScene(self.scene)
